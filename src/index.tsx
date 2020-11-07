cfcfc95d653130c202e20bd0cc341e9939484048
muco <mucorolle@gmail.com>
2020-11-08 01:11:06 +0200

    setup: Initial setup.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,19 @@
+import { App } from 'app';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { render } from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const RootComponent = () => (
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
+
+const rootElement = document.querySelector('#root');
+
+render(<RootComponent />, rootElement);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(console.log);

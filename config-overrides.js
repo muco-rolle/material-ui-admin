0b56929ca5ab1fbc1b37d16ddc124d6d67997d06
Muco Rolle Tresor <mucotresor@gmail.com>
2020-11-28 16:05:26 +0200

    setup: Add theme and override typography.

---
+++ config-overrides.js
@@ -0,0 +1,8 @@
+const { override } = require('customize-cra');
+const { addReactRefresh } = require('customize-cra-react-refresh');
+const { alias, configPaths } = require('react-app-rewire-alias');
+
+module.exports = override(
+    addReactRefresh(),
+    alias({ ...configPaths('tsconfig.paths.json') })
+);
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,8 +1,95 @@
 import { createMuiTheme } from '@material-ui/core';
-import { palette } from './palette';
-import { typography } from './typography';
+
+// import overrides from './overrides';
 
 export const theme = createMuiTheme({
-    palette,
-    typography,
+    palette: {
+        primary: {
+            '50': '#ffe8fd',
+            '100': '#efc3eb',
+            '200': '#e29ddc',
+            '300': '#d676cc',
+            '400': '#c950bd',
+            '500': '#af36a3',
+            '600': '#892980',
+            '700': '#631d5c',
+            '800': '#3d1039',
+            '900': '#190316',
+        },
+
+        text: {
+            primary: '#2D3748',
+        },
+
+        grey: {
+            '50': '#F7FAFC',
+            '100': '#EDF2F7',
+            '200': '#E2E8F0',
+            '300': '#CBD5E0',
+            '400': '#A0AEC0',
+            '500': '#718096',
+            '600': '#4A5568',
+            '700': '#2D3748',
+            '800': '#1A202C',
+            '900': '#171923',
+        },
+    },
+    typography: {
+        fontFamily: `'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif`,
+        h1: {
+            fontWeight: 700,
+            fontSize: '4.209rem',
+            letterSpacing: '-0.24px',
+        },
+        h2: {
+            fontWeight: 700,
+            fontSize: '3.157rem',
+            letterSpacing: '-0.24px',
+        },
+        h3: {
+            fontWeight: 700,
+            fontSize: '2.369rem',
+            letterSpacing: '-0.06px',
+        },
+        h4: {
+            fontWeight: 700,
+            fontSize: '1.777rem',
+            letterSpacing: '-0.06px',
+        },
+        h5: {
+            fontWeight: 700,
+            fontSize: '1.333rem',
+            letterSpacing: '-0.05px',
+        },
+        h6: {
+            fontWeight: 700,
+            fontSize: '1rem',
+            letterSpacing: '-0.05px',
+        },
+        subtitle1: {
+            fontSize: '1rem',
+            letterSpacing: '-0.05px',
+        },
+        subtitle2: {
+            fontWeight: 400,
+            fontSize: '0.071rem',
+            letterSpacing: '-0.05px',
+        },
+        body1: {
+            fontSize: '1rem',
+            letterSpacing: '-0.05px',
+        },
+        body2: {
+            fontSize: '0.071rem',
+            letterSpacing: '-0.04px',
+        },
+        button: {
+            textTransform: 'none',
+        },
+        caption: {
+            fontSize: '11px',
+            letterSpacing: '0.33px',
+        },
+    },
+    // overrides,
 });
